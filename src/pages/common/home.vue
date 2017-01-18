a99ddfe4f5a9cdd8f3d49d025cee534e400456c8
vvliebe <vvliebe@gmail.com>
2017-01-18 11:18:17 +0800

    添加网站logo

--- src/pages/common/home.vue
+++ src/pages/common/home.vue
@@ -1,21 +1,34 @@
 <template lang="html">
   <section id='workspace'>
-    <el-menu theme="dark" :default-active="activeMenu" router class="menu-bar">
-      <template v-for="(route, index) in $router.options.routes[$router.options.routes.length - 2].children">
-        <template v-if="route.isSubmenu">
-          <el-submenu :index="route.name">
-            <template slot="title"><i :class="route.iconClass"></i>{{route.name}}</template>
-            <el-menu-item :index="cRoute.name" v-for="(cRoute, cIndex) in route.children" :route="cRoute">{{cRoute.name}}</el-menu-item>
-          </el-submenu>
+
+    <div class="nav">
+      <div class="nav-header">
+        <img src="http://ojygpugo6.bkt.clouddn.com/images/workspace-avatar.jpeg">
+      </div>
+
+      <el-menu :default-active="activeMenu" router class="menu-bar">
+        <template v-for="(route, index) in $router.options.routes[$router.options.routes.length - 2].children">
+          <template v-if="route.isSubmenu">
+            <el-submenu :index="route.name">
+              <template slot="title"><i :class="route.iconClass"></i>{{route.name}}</template>
+              <el-menu-item :index="cRoute.name" v-for="(cRoute, cIndex) in route.children" :route="cRoute">{{cRoute.name}}</el-menu-item>
+            </el-submenu>
+          </template>
+          <template v-if="route.isMenuItem">
+            <el-menu-item :index="route.name" :route="route"><i :class="route.iconClass"></i>{{route.name}}</el-menu-item>
+          </template>
         </template>
-        <template v-if="route.isMenuItem">
-          <el-menu-item :index="route.name" :route="route"><i :class="route.iconClass"></i>{{route.name}}</el-menu-item>
-        </template>
-      </template>
-    </el-menu>
+      </el-menu>
+
+      <div class="nav-footer">
+
+      </div>
+    </div>
+
     <div class="main-content">
       <router-view></router-view>
     </div>
+
   </section>
 </template>
 
@@ -29,15 +42,37 @@ export default {
 }
 </script>
 
-<style lang="scss">
+<style lang="scss" scoped>
   #workspace {
     display: flex;
     font-family: 'Avenir', Helvetica, Arial, sans-serif;
     height: 100vh;
 
-    .menu-bar {
-      width: 200px;
+    .nav {
+      display: flex;
+      flex-direction: column;
+      width: 250px;
       height: 100%;
+      background: #EFF2F7;
+
+      .nav-header {
+        display: flex;
+        height: 150px;
+        align-items: center;
+        justify-content: center;
+        img {
+          width: 100px;
+          height: 100px;
+          border-radius: 50%;
+        }
+      }
+
+      .menu-bar {
+        flex: 1;
+      }
+      .nav-footer {
+        height: 100px;
+      }
     }
 
     .main-content {
