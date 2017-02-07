40f36a29c8e170febe0c09f46dbdeee6ef24056c
vvliebe <vvliebe@gmail.com>
2017-02-07 17:34:49 +0800

    add v-modal component

--- src/components/v-modal/index.vue
+++ src/components/v-modal/index.vue
@@ -1,11 +1,123 @@
 <template lang="html">
-  <div class="v-modal-wrapper">components</div>
+  <transition name="fade">
+    <div class="v-modal-wrapper" v-show="visible">
+      <div class="modal-mask" @click="close"></div>
+      <transition :name="type">
+        <div v-show="visible" class="modal-box">
+        </div>
+      </transition>
+    </div>
+</transition>
 </template>
 
 <script>
 export default {
+  props: {
+    visible: {
+      type: Boolean,
+      required: true
+    },
+    type: {
+      type: String,
+      default: 'fade'
+    },
+    close: {
+      type: Function,
+      required: true
+    }
+  }
 }
 </script>
 
-<style lang="css">
+<style lang="scss">
+.v-modal-wrapper {
+  transform-style: preserve-3d;
+  perspective: 1500px;
+  position: fixed;
+  left: 0;
+  right: 0;
+  top: 0;
+  bottom: 0;
+  .modal-mask {
+    position: absolute;
+    left: 0;
+    right: 0;
+    top: 0;
+    bottom: 0;
+    background: rgba(0, 0, 0, .45);
+  }
+  .modal-box {
+    position: absolute;
+    left: 0;
+    right: 0;
+    top: 0;
+    bottom: 0;
+    margin: auto;
+    width: 600px;
+    height: 400px;
+    background: rgb(255, 255, 255);
+  }
+}
+.fade-enter-active, .fade-leave-active {
+  transition: all .5s;
+}
+.fade-enter, .fade-leave-active {
+  opacity: 0;
+}
+
+.zoom-enter-active, .zoom-leave-active {
+  transition: all .5s;
+}
+.zoom-enter, .zoom-leave-active {
+  transform: scale(.1);
+}
+
+.flip-enter-active, .flip-leave-active {
+  transition: all .5s;
+}
+.flip-enter, .flip-leave-active {
+  transform: rotateX(75deg);
+}
+
+.door-enter-active, .door-leave-active {
+  transition: all .5s;
+}
+.door-enter, .door-leave-active {
+  transform: rotateY(75deg);
+}
+
+.rotate-enter-active, .rotate-leave-active {
+  transition: all .5s;
+}
+.rotate-enter, .rotate-leave-active {
+  transform: rotateZ(120deg);
+}
+
+.slideUp-enter-active, .slideUp-leave-active {
+  transition: all .5s;
+}
+.slideUp-enter, .slideUp-leave-active {
+  transform: translate(0, 100px);
+}
+
+.slideDown-enter-active, .slideDown-leave-active {
+  transition: all .5s;
+}
+.slideDown-enter, .slideDown-leave-active {
+  transform: translate(0, -100px);
+}
+
+.slideLeft-enter-active, .slideLeft-leave-active {
+  transition: all .5s;
+}
+.slideLeft-enter, .slideLeft-leave-active {
+  transform: translate(-100px, 0);
+}
+
+.slideRight-enter-active, .slideRight-leave-active {
+  transition: all .5s;
+}
+.slideRight-enter, .slideRight-leave-active {
+  transform: translate(100px, 0);
+}
 </style>
--- src/pages/demos/v-modal/index.vue
+++ src/pages/demos/v-modal/index.vue
@@ -1,9 +1,14 @@
 <template lang="html">
   <div class="demo-v-modal-wrapper">
     <div class="btn-grid">
-      <el-button>testing</el-button>
+      <div v-for="type in types">
+        <el-button @click="openModal(type)">{{type}}</el-button>
+      </div>
     </div>
-    <v-modal></v-modal>
+    <v-modal
+      :type="modalType"
+      :visible="modalVisible"
+      :close="closeModal"></v-modal>
   </div>
 </template>
 
@@ -14,20 +19,62 @@ import vModal from 'components/v-modal'
 
 Vue.use(Button)
 export default {
+  data() {
+    return {
+      types: [
+        'zoom',
+        'fade',
+        'flip',
+        'door',
+        'rotate',
+        'slideUp',
+        'slideDown',
+        'slideLeft',
+        'slideRight'
+      ],
+      modalType: 'fade',
+      modalVisible: false
+    }
+  },
   components: {
     vModal
+  },
+  methods: {
+    openModal(type) {
+      this.modalType = type
+      this.modalVisible = true
+    },
+    closeModal() {
+      this.modalVisible = false
+    }
   }
 }
 </script>
 
 <style lang="scss" scoped>
   .demo-v-modal-wrapper {
+    position: relative;
     width: 100%;
     height: 100%;
     background: #58B7FF;
     .btn-grid {
+      position: absolute;
+      left: 50%;
+      top: 50%;
+      transform: translate(-50%, -50%);
+      width: 400px;
+      height: 200px;
       display: flex;
       flex-wrap: wrap;
+      div {
+        display: flex;
+        width: 33.3%;
+        justify-content: center;
+        button {
+          width: 100px;
+          height: 40px;
+        }
+      }
     }
   }
 </style>
